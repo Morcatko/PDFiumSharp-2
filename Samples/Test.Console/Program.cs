ac9f564e04599a4835a863f2aa9f55a05157f0c5
Ondra <ondrej.morsky@hotmail.com>
2020-06-18 09:33:59 +0200

    Linux

--- Samples/Test.Console/Program.cs
+++ Samples/Test.Console/Program.cs
@@ -1,31 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using PDFiumSharp;
+﻿using PDFiumSharp;
+using System;
 using System.IO;
+using System.Reflection;
 
-namespace TestConsole
+namespace Test
 {
 	class Program
 	{
 		static void Main(string[] args)
 		{
-			using (var doc = new PdfDocument("TestDoc.pdf", "password"))
+			var exeDir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+			using (var doc = new PdfDocument(exeDir + "/TestDoc.pdf", "password"))
 			{
-				int i = 0;
-				foreach (var page in doc.Pages)
-				{
-					using (var bitmap = new PDFiumBitmap((int)page.Width, (int)page.Height, true))
-					using (var stream = new FileStream($"{i++}.bmp", FileMode.Create))
-					{
-						page.Render(bitmap);
-						bitmap.Save(stream);
-					}
-				}
+				Console.WriteLine($"Pages: {doc.Pages.Count}");
 			}
-			Console.ReadKey();
 		}
 	}
 }
